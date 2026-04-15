341d5424e32814a0b8ed55c75b06427ac216bf4b
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-04-15 10:00:00 +0530

    Updated Rental

--- src/model/Rental.java
+++ src/model/Rental.java
@@ -6,12 +6,40 @@ public class Rental {
     private Customer customer;
     private Vehicle vehicle;
     private int days;
+    private double totalCost;
+    private String rentalDate;
+    private String returnDate;
 
     public Rental(int rentalId, Customer customer, Vehicle vehicle, int days) {
         this.rentalId = rentalId;
         this.customer = customer;
         this.vehicle = vehicle;
         this.days = days;
+        this.totalCost = calculateTotal();
+    }
+
+    public int getRentalId() {
+        return rentalId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public String getRentalDate() {
+        return rentalDate;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
     }
 
     public double calculateTotal() {
